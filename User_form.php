9f3e81753d5dc1dcee1b4be440d0d2e7a2a8a54c
Raiko Lepik <raiklep@greeny.cs.tlu.ee>
2015-09-07 15:33:30 +0300

    new file

--- User_form.php
+++ User_form.php
@@ -1,8 +1,20 @@
 <?php
 
 		// user_form.php
-	
-	
+		// jutumärkide vahele input elemendi väärtus
+		
+		//echo $_POST["email"];
+		
+		//echo $_POST["Password"];
+		
+		$email_error = "";
+		
+		//kontrolli ainult siis, kui kasutaja vajutab logi sisse nuppu
+		if($_SERVER["REQUEST_METHOD"] == "POST"){
+			if(empty($_POST["email"]))	{
+				$email_error = "ei saa olla tühi";
+			}
+		}
 ?>
 <html>
 	<head>
@@ -11,11 +23,14 @@
 	<body>
 	
 		<h2>Login</h2>
-		<input type="Email" placeholder="E-post"> <br> <br> <br>
-		<input type="Password" placeholder="Password">	
-		<h2>Create user</h2>
+		<form action="User_form.php" method="post">
+		
+		<input name="email" type="Email" placeholder="E-post"> <?php echo $email_error; ?> <br> <br> 
+		<input name="Password" type="Password" placeholder="Password">	
 		
 		<input type ="submit" value ="logi sisse">
+		</form>
+		<h2>Create user</h2>
 		
 	</body>
 </html>

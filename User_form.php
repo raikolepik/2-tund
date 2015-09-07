276df1981221c3a5dc87f8d36b5e3370857b6027
Raiko Lepik <raiklep@greeny.cs.tlu.ee>
2015-09-07 14:57:49 +0300

    Login page

---
+++ User_form.php
@@ -0,0 +1,21 @@
+<?php
+
+		// user_form.php
+	
+	
+?>
+<html>
+	<head>
+	 <title>User login page</title>
+	</head>
+	<body>
+	
+		<h2>Login</h2>
+		<input type="Email" placeholder="E-post"> <br> <br> <br>
+		<input type="Password" placeholder="Password">	
+		<h2>Create user</h2>
+		
+		<input type ="submit" value ="logi sisse">
+		
+	</body>
+</html>

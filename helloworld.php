feacc2de7d388087ee2485973644fca78adc369e
Raiko Lepik <raiklep@greeny.cs.tlu.ee>
2015-09-07 13:46:54 +0300

    New file

--- helloworld.php
+++ helloworld.php
@@ -1,8 +1,27 @@
 <?php
 
-$first_name = "Raiko";
-$last_name = "Lepik";
+	$first_name = "Raiko";
+	$last_name = "Lepik";
 
-//trükib välja
-echo $first_name." ".$last_name;
+	//trükib välja
+	echo $first_name." ".$last_name;
+
+?>
+
+<br>
+
+<?php
+
+	$age = 19;
+	
+	// if (loogikatehe) ){tõene} else (väär)
+	if($age < 19) { 
+		echo "vana";
+		
+		
+	} else {
+		echo "täisealine";
+	}	
+
+
 ?>

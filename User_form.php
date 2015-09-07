08622b8b8a325523358b1e7d90791441e82493dd
Raiko Lepik <raiklep@greeny.cs.tlu.ee>
2015-09-07 15:42:38 +0300

    Läbiii

--- User_form.php
+++ User_form.php
@@ -13,7 +13,14 @@
 		if($_SERVER["REQUEST_METHOD"] == "POST"){
 			if(empty($_POST["email"]))	{
 				$email_error = "ei saa olla tühi";
-			}
+			}	
+			if(empty($_POST["Password"]))
+		$password_error = "Teie parooli lünk on tühi";
+	
+				//parool ei saa olla tühi, kontrollime pikkust
+				if(strlen($_POST["Password"]) < 8 ) {
+				
+					$password_error = "Peab olema vähemalt 8 sümbolit pikk";
 		}
 ?>
 <html>
@@ -25,7 +32,7 @@
 		<h2>Login</h2>
 		<form action="User_form.php" method="post">
 		
-		<input name="email" type="Email" placeholder="E-post"> <?php echo $email_error; ?> <br> <br> 
+		<input name="email" type="Email" placeholder="E-post">* <?php echo $email_error; ?> <br> <br> 
 		<input name="Password" type="Password" placeholder="Password">	
 		
 		<input type ="submit" value ="logi sisse">
